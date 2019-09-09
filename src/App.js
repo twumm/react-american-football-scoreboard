872f6b2e73cf9a5eba3b0ba504f18f902d2749fb
Martin Twum Mensah <martint.mensah@gmail.com>
2019-09-09 12:21:58 +0000

    feature: Set homeScore and awayScore in application

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
-  
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -18,12 +18,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
